bbb1582ec1fd61bb0018be4b153b32f7b5bd1d22
Tejas Binu <tbinu@opentext.com>
2025-01-18 13:39:42 +0530

    Completed basic web application from scratch

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
+<aside>
+💡
+
+https://defn.io/2018/02/25/web-app-from-scratch-01/
+
+</aside>
+
+<aside>
+💡
+
+### PowerShell commands
+
+```powershell
+
+Invoke-WebRequest -Uri "http://127.0.0.1:9000/"
+```
+
+```powershell
+Test-NetConnection -ComputerName 127.0.0.1 -Port 9000
+```
+
+</aside>
+
 ### Request format
 
 - request line \r\n
@@ -44,3 +67,119 @@ with socket.socket() as server_socket:
 ```
 
 ### Request Abstraction
+
+```python
+import typing
+
+def iter_lines(socket_instance: socket.socket, buffer_size: int = 16_384) -> typing.Generator[bytes, None, bytes]:
+    buffer = b""
+    while True:
+        data = socket_instance.recv(buffer_size)
+        if not data:
+            return b""
+        buffer += data
+        while True:
+            try:
+                i = buffer.index(b"\r\n")
+                line, buffer = buffer[:i], buffer[i + 2:]
+                if not line:
+                    return buffer
+                yield line
+            except IndexError:
+                break
+
+```
+
+```python
+with socket.socket() as server_socket:
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(0)
+
+    print(f"Listening on {HOST}:{PORT}...")
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+
+        print(f"New connection from {client_address}")
+
+        with client_socket:
+            for request_line in iter_lines(client_socket):
+                print(request_line)
+            client_socket.sendall(RESPONSE)
+```
+
+### A file server
+
+```python
+import mimetypes
+import os
+import socket
+import typing
+
+SERVER_ROOT = os.path.abspath("www")
+
+FILE_RESPONSE_TEMPLATE = """\
+HTTP/1.1 200 OK
+Content-type: {content_type}
+Content-length: {content_length}
+
+""".replace("\n", "\r\n")
+
+def serve_file(sock: socket.socket, path: str) -> None:
+    """Given a socket and the relative path to a file (relative to
+    SERVER_SOCK), send that file to the socket if it exists.  If the
+    file doesn't exist, send a "404 Not Found" response.
+    """
+    if path == "/":
+        path = "/index.html"
+
+    abspath = os.path.normpath(os.path.join(SERVER_ROOT, path.lstrip("/")))
+    if not abspath.startswith(SERVER_ROOT):
+        sock.sendall(NOT_FOUND_RESPONSE)
+        return
+
+    try:
+        with open(abspath, "rb") as f:
+            stat = os.fstat(f.fileno())
+            content_type, encoding = mimetypes.guess_type(abspath)
+            if content_type is None:
+                content_type = "application/octet-stream"
+
+            if encoding is not None:
+                content_type += f"; charset={encoding}"
+
+            response_headers = FILE_RESPONSE_TEMPLATE.format(
+                content_type=content_type,
+                content_length=stat.st_size,
+            ).encode("ascii")
+
+            sock.sendall(response_headers)
+            sock.sendfile(f)
+    except FileNotFoundError:
+        sock.sendall(NOT_FOUND_RESPONSE)
+        return
+```
+
+```python
+with socket.socket() as server_sock:
+    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_sock.bind((HOST, PORT))
+    server_sock.listen(0)
+    print(f"Listening on {HOST}:{PORT}...")
+
+    while True:
+        client_sock, client_addr = server_sock.accept()
+        print(f"Received connection from {client_addr}...")
+        with client_sock:
+            try:
+                request = Request.from_socket(client_sock)
+                if request.method != "GET":
+                    client_sock.sendall(METHOD_NOT_ALLOWED_RESPONSE)
+                    continue
+
+                serve_file(client_sock, request.path)
+            except Exception as e:
+                print(f"Failed to parse request: {e}")
+                client_sock.sendall(BAD_REQUEST_RESPONSE)
+```
--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import mimetypes
+import os
 import socket
+import typing
 
+# Constants
+SERVER_ROOT = os.path.abspath("www")
 HOST = "127.0.0.1"
 PORT = 9000
+
+FILE_RESPONSE_TEMPLATE = """\
+HTTP/1.1 200 OK
+Content-type: {content_type}
+Content-length: {content_length}
+
+""".replace("\n", "\r\n")
 
 RESPONSE = b"""\
 HTTP/1.1 200 OK
@@ -10,6 +22,98 @@ Content-length: 15
 
 <h1>Hello!</h1>""".replace(b"\n", b"\r\n")
 
+BAD_REQUEST_RESPONSE = b"""\
+HTTP/1.1 400 Bad Request
+"""
+
+NOT_FOUND_RESPONSE = b"""\
+HTTP/1.1 404 Not Found
+Content-type: text/plain
+Content-length: 9
+
+Not Found""".replace(b"\n", b"\r\n")
+
+METHOD_NOT_ALLOWED_RESPONSE = b"""\
+HTTP/1.1 405 Method Not Allowed
+Content-type: text/plain
+Content-length: 17
+
+Method Not Allowed""".replace(b"\n", b"\r\n")
+
+# /Constants
+
+# Methods
+def iter_lines(socket_instance: socket.socket, buffer_size: int = 16_384) -> typing.Generator[bytes, None, bytes]:
+    buffer = b""
+    while True:
+        data = socket_instance.recv(buffer_size)
+        if not data:
+            return b""
+        buffer += data
+        while True:
+            try:
+                i = buffer.index(b"\r\n")
+                line, buffer = buffer[:i], buffer[i + 2:]
+                if not line:
+                    return buffer
+                yield line
+            except IndexError:
+                break
+
+def server_file(socket_instance: socket.socket, path: str) -> None:
+    if path == '/':
+        path = "/index.html"
+    abspath = os.path.normpath(os.path.join(SERVER_ROOT, path.lstrip("/")))
+    if not abspath.startswith(SERVER_ROOT):
+        socket_instance.sendall(NOT_FOUND_RESPONSE)
+        return
+    try:
+        with open(abspath, "rb") as f:
+            stat = os.fstat(f.fileno())
+            content_type, encoding = mimetypes.guess_type(abspath)
+            if content_type is None:
+                content_type = "application/octet-stream"
+            if encoding is not None:
+                content_type += f"; charset={encoding}"
+            response_headers = FILE_RESPONSE_TEMPLATE.format(
+                content_type=content_type,
+                content_length=stat.st_size,
+            ).encode("ascii")
+            socket_instance.sendall(response_headers)
+            socket_instance.sendfile(f)
+    except FileNotFoundError:
+        socket_instance.sendall(NOT_FOUND_RESPONSE)
+        return
+# /Methods
+
+# Classes
+class Request(typing.NamedTuple):
+    method: str
+    path: str
+    headers: typing.Mapping[str, str]
+
+    @classmethod
+    def from_socket(cls, socket_instance: socket.socket) -> "Request":
+        lines = iter_lines(socket_instance)
+        try:
+            request_line = next(lines).decode("ascii")
+        except StopIteration:
+            raise ValueError("Request line missing.")
+        try:
+            method, path, _ = request_line.split(" ")
+        except ValueError:
+            raise ValueError(f"Malformed request line {request_line!r}.")
+        headers = {}
+        for line in lines:
+            try:
+                name, _, value = line.decode("ascii").partition(":")
+                headers[name.lower()] = value.lstrip()
+            except ValueError:
+                raise ValueError(f"Malformed header line {line!r}.")
+        return cls(method=method.upper(), path=path, headers=headers)
+# /Classes
+
+# Main
 with socket.socket() as server_socket:
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((HOST, PORT))
@@ -23,4 +127,13 @@ with socket.socket() as server_socket:
         print(f"New connection from {client_address}")
 
         with client_socket:
-            client_socket.sendall(RESPONSE)
+            try:
+                request = Request.from_socket(client_socket)
+                if request.method != "GET":
+                    client_socket.sendall(NOT_FOUND_RESPONSE)
+                    continue
+                server_file(client_socket, request.path)
+            except Exception as e:
+                print(f"Failed to parse request: {e}")
+                client_socket.sendall(BAD_REQUEST_RESPONSE)
+# /Main
